d7af0a2c482fb467062cf7f6f5056f0357bd00f0
Ardhendu Barge <bargea@mcmaster.ca>
2018-10-10 20:05:23 -0400

    added changes

---
+++ src/config.js
@@ -0,0 +1,6 @@
+
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/023a14b7-4d8e-4b1b-b9a1-192220a48a3c/token";
+
+const instanceLocator = "v1:us1:023a14b7-4d8e-4b1b-b9a1-192220a48a3c";
+
+export {instanceLocator, tokenUrl};
